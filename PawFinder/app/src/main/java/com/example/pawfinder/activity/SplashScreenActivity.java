8d85c0ccb262734e69ee7a57e5cbf9eb610d92dd
grujcicnikola <grujcicnikola@uns.ac.rs>
2020-04-29 21:15:54 +0200

    prvi komit

---
+++ PawFinder/app/src/main/java/com/example/pawfinder/activity/SplashScreenActivity.java
@@ -0,0 +1,29 @@
+package com.example.pawfinder.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.pawfinder.MainActivity;
+import com.example.pawfinder.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        int SPLASH_TIME_OUT = 3000;
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                finish(); // da ne bi mogao da ode back na splash
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
