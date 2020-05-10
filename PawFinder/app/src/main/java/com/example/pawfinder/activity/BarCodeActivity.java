bd8827cbdf9faf5bbd28688958cd9ba8c24f1b39
grujcicnikola <grujcicnikola@uns.ac.rs>
2020-05-10 20:36:28 +0200

    popravka permisija kod QR koda

--- PawFinder/app/src/main/java/com/example/pawfinder/activity/BarCodeActivity.java
+++ PawFinder/app/src/main/java/com/example/pawfinder/activity/BarCodeActivity.java
@@ -2,6 +2,7 @@ package com.example.pawfinder.activity;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.SparseArray;
 import android.view.MenuItem;
@@ -57,8 +58,11 @@ public class BarCodeActivity  extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (intentData.length() > 0) {
-                    Intent intent = new Intent(getApplicationContext(), ScannedDataActivity.class);
-                    intent.putExtra("scanned_data",intentData);
+                    //Intent intent = new Intent(getApplicationContext(), ScannedDataActivity.class);
+                    //intent.putExtra("scanned_data",intentData);
+                    //startActivity(intent);
+                    Uri uri = Uri.parse(intentData); // missing 'http://' will cause crashed
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                     startActivity(intent);
                 }
             }
@@ -67,7 +71,7 @@ public class BarCodeActivity  extends AppCompatActivity {
 
     private void initialiseDetectorsAndSources() {
 
-        Toast.makeText(getApplicationContext(), "Barcode scanner started", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(getApplicationContext(), "Barcode scanner started", Toast.LENGTH_SHORT).show();
         barcodeDetector = new BarcodeDetector.Builder(this)
                 .setBarcodeFormats(Barcode.ALL_FORMATS)
                 .build();
@@ -80,12 +84,17 @@ public class BarCodeActivity  extends AppCompatActivity {
         surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
-                try {
+                try {//checkSelfPermission proverava da li je Permission granted or denied
                     if (ActivityCompat.checkSelfPermission(BarCodeActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
                         cameraSource.start(surfaceView.getHolder());
                     } else {
+                        if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+                            Toast.makeText(getApplicationContext(), "Camera permission is needed to scan QR code!",
+                                    Toast.LENGTH_SHORT).show();
+                        }
                         ActivityCompat.requestPermissions(BarCodeActivity.this, new
-                                String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+                                    String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+
                     }
 
                 } catch (IOException e) {
@@ -101,6 +110,8 @@ public class BarCodeActivity  extends AppCompatActivity {
             public void surfaceDestroyed(SurfaceHolder holder) {
                 cameraSource.stop();
             }
+
+
         });
 
 
@@ -138,6 +149,88 @@ public class BarCodeActivity  extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CAMERA_PERMISSION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                    initialiseBarcoder();
+                    try {
+                        initialiseCamera();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    //surfaceView = findViewById(R.id.surfaceView);
+                    //initialiseDetectorsAndSources();
+
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(getApplicationContext(), "Camera permission is needed to scan QR code!",
+                            Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+
+    public void initialiseBarcoder(){
+        barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.ALL_FORMATS)
+                .build();
+
+
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+                Toast.makeText(getApplicationContext(), "To prevent memory leaks barcode scanner has been stopped", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+                if (barcodes.size() != 0) {
+                    txtBarcodeValue.post(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            if (barcodes.valueAt(0).email != null) {
+                                txtBarcodeValue.removeCallbacks(null);
+                                intentData = barcodes.valueAt(0).email.address;
+                                txtBarcodeValue.setText(intentData);
+                                isEmail = true;
+                                btnAction.setText("ADD CONTENT TO THE MAIL");
+                            } else {
+                                isEmail = false;
+                                btnAction.setText("LAUNCH URL");
+                                intentData = barcodes.valueAt(0).displayValue;
+                                txtBarcodeValue.setText(intentData);
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+    }
+
+    public void initialiseCamera() throws IOException {
+        cameraSource = new CameraSource.Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(1920, 1080)
+                .setAutoFocusEnabled(true) //you should add this feature
+                .build();
+        cameraSource.start(surfaceView.getHolder());
+    }
     @Override
     protected void onPause() {
         super.onPause();
